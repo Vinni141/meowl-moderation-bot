57ee767edd47c1888cad749634ce775583375e70
Vinni141 <vincthalheim@gmail.com>
2026-05-29 12:08:14 +0200

    Remove Discord channel type workaround

--- src/types/discord-channel.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Message, MessageCreateOptions } from 'discord.js';
-import 'discord.js';
-
-declare module 'discord.js' {
-  interface PartialGroupDMChannel {
-    send(options: MessageCreateOptions): Promise<Message>;
-  }
-}
